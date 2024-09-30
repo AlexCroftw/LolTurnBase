4501cc80e42bbd4c190557ea96ed684499891c7b
Lexi Cro <justlexcroftw@gmail.com>
2024-09-30 16:55:40 +0300

    Added POST Method .

--- LolTurnBase/Controllers/ChampionController.cs
+++ LolTurnBase/Controllers/ChampionController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using LolTurnBase.Data;
 using LolTurnBase.Models;
+using LolTurnBase.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
@@ -27,7 +28,7 @@ namespace LolTurnBase.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllChampions()
+        public async Task<ActionResult<APIResponse>> GetAllChampions()
         {
             List<Champion> champList = await _db.Champion.ToListAsync();
 
@@ -37,6 +38,7 @@ namespace LolTurnBase.Controllers
             return Ok(_response);
 
         }
+
         [HttpGet("{id:int}", Name ="Get Champion")]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -44,7 +46,7 @@ namespace LolTurnBase.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
-        public async Task<IActionResult> GetOneChampion(int id)
+        public async Task<ActionResult<APIResponse>> GetOneChampion(int id)
         {
             if (id == null || id == 0) 
             {
@@ -53,12 +55,39 @@ namespace LolTurnBase.Controllers
             }
             var champ = await _db.Champion.FirstOrDefaultAsync(x => x.Id == id);
             _response.Result = _mapper.Map<Champion>(champ);
+
             if (_response.Result == null) 
             {
                 ModelState.AddModelError("ErrorMessages", "Champion does not exist");
                 return NotFound(ModelState);
             }
             return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateChampion([FromBody]ChampionDTO championDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                if (championDTO == null)
+                {
+                    ModelState.AddModelError("ErrorMessages", "Item is null and void");
+                    return BadRequest(ModelState);
+                }
+
+                var champion = _mapper.Map<Champion>(championDTO);
+                await _db.Champion.AddAsync(champion);
+                _db.SaveChanges();
+                return Ok(_response);
+            }
+            else 
+            {
+                return BadRequest(ModelState);
+            }
+           
         }
     }
 }
